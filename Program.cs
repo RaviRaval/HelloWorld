edf4f8bc30a6ef7b3e4909d09a41e2bd4536787d
RaviRaval <39405963+RaviRaval@users.noreply.github.com>
2018-05-19 06:31:41 -0700

    Update Program.cs
    Learning ASP.NET CORE

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Ravi!");
+            Console.WriteLine("Hello Ravi! Welcome to ASP.NET CODE");
         }
     }
 }
